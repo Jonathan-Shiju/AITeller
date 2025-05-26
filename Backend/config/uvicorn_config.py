fa36e6eb1202059768046a0d31943f5370a9dfa0
Jonathan Shiju <jonathan@thebutterflyculture.com>
2025-05-26 20:44:31 +0530

    Update uvicorn_config.py

--- Backend/config/uvicorn_config.py
+++ Backend/config/uvicorn_config.py
@@ -1,9 +1,11 @@
 import uvicorn
 from Backend.config.app_factory import create_app
+from asgiref.wsgi import WsgiToAsgi
 
 def run_uvicorn():
     app = create_app()
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    asgi_app = WsgiToAsgi(app)
+    uvicorn.run(asgi_app, host='0.0.0.0', port=8000)
 
 if __name__ == "__main__":
     run_uvicorn()
